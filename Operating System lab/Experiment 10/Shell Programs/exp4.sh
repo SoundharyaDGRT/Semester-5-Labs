4073cb16f7fd1a1aeeda4c2a2d525f8538aef947
SoundharyaDGRT <soundnext123@gmail.com>
2020-11-25 11:15:58 +0530

    Create exp4.sh

---
+++ Operating System lab/Experiment 10/Shell Programs/exp4.sh
@@ -0,0 +1,15 @@
+# showing variuos informations in a system
+
+echo "SYSTEM INFORMATION"
+echo "Hello .$LOGNAME"
+echo "Current Date is = $(date)"
+echo "User is 'who I am'"
+echo "Current Dirctory = $(pwd)"
+echo "Network Name and Node Name = $(uname -n)"
+echo "Kernel Name =$(uname -s)"
+echo "Kernel Version =$(uname -v)"
+echo "Kernel Release =$(uname -r)"
+echo "Kernel OS =$(uname-o)"
+echo "Processor Type $(uname -p)"
+echo "Kernel Machine Information =$(uname -m)"
+echo "All Information =$(uname -a)"
